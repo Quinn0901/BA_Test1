a133e8ec9a15b687c7a591bd12e02f882739d4f3
qun <xiongqun@baidu.com>
2014-05-07 14:24:52 +0800

    Add print "commit"

---
+++ my-app-sample/src/main/java/com/mycompany/app/my_app_sample/App.java
@@ -0,0 +1,14 @@
+package com.mycompany.app.my_app_sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Test Commit" );
+    }
+}
